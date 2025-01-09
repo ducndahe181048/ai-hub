ce0295e274a1bb2e743da3292075ade92c7cd00b
ducndahe181048 <144119087+ducndahe181048@users.noreply.github.com>
2025-01-09 17:08:15 +0700

    feat: upload file, audio recorder

--- src/components/SuDungDichVu.js
+++ src/components/SuDungDichVu.js
@@ -5,11 +5,55 @@ import audio2 from '../assets/FPTOpenSpeechData_Set001_V0.1_000001.wav'
 
 function SuDungDichVu() {
     const [audioSrc, setAudioSrc] = useState('');
+    const [isRecording, setIsRecording] = useState(false);
 
     const handleAudioChange = (src) => {
         setAudioSrc(src);
     }
-    
+
+    const handleFileUpload = (e) => {
+        const file = e.target.files[0];
+        if (file && file.type === 'audio/wav') {
+            const src = URL.createObjectURL(file);
+            setAudioSrc(src);
+        } else {
+            alert('File không hợp lệ! Vui lòng chọn file có định dạng .wav.');
+        }
+    }
+
+    const handleRecordAudio = () => {
+        navigator.mediaDevices.getUserMedia({ 
+            // audio: true,
+            audio: {
+                echoCancellation: true,
+                noiseSuppression: true,
+                sampleRate: 16000
+            }
+         })
+            .then(stream => {
+                const mediaRecorder = new MediaRecorder(stream);
+                mediaRecorder.start();
+
+                const audioChunks = [];
+                mediaRecorder.addEventListener('dataavailable', event => {
+                    audioChunks.push(event.data);
+                });
+
+                mediaRecorder.addEventListener('stop', () => {
+                    const audioBlob = new Blob(audioChunks);
+                    const audioUrl = URL.createObjectURL(audioBlob);
+                    setAudioSrc(audioUrl);
+                });
+
+                setTimeout(() => {
+                    mediaRecorder.stop();
+                }, 5000); // Record for 5 seconds
+            })
+            .catch(error => {
+                alert('Hãy bật quyền truy cập microphone để có thể sử dụng tính năng này!', error);
+            });
+    };
+
     return (
         <div style={{ padding: '20px', textAlign: 'left', margin: '20px' }}>
             <Container>
@@ -34,8 +78,24 @@ function SuDungDichVu() {
                             <Col md={5}>
                                 <h6>Chọn nguồn âm thanh - File (.wav) upload dung lượng không quá 20MB</h6>
                                 <div style={{ padding: '10px' }}>
-                                    <Button style={{ backgroundColor: 'rgb(0, 200, 255)', border: 'none', margin: '10px' }} variant="primary">Ghi âm giọng nói</Button>
-                                    <Button style={{ backgroundColor: 'rgb(0, 200, 255)', border: 'none', margin: '10px' }} variant="primary">Tải file âm thanh</Button>
+                                    <Button onClick={() => handleRecordAudio()} style={{ backgroundColor: 'rgb(0, 200, 255)', border: 'none', margin: '10px' }} variant="primary">
+                                        {isRecording ? 'Dừng ghi âm' : 'Ghi âm giọng nói'}
+                                    </Button>
+                                    <input
+                                        type="file"
+                                        style={{ display: 'none' }}
+                                        id="upload-wav"
+                                        onChange={handleFileUpload}
+                                    />
+                                    <label htmlFor="upload-wav">
+                                        <Button
+                                            as="span"
+                                            style={{ backgroundColor: 'rgb(0, 200, 255)', border: 'none', margin: '10px' }}
+                                            variant="primary"
+                                        >
+                                            Tải file âm thanh
+                                        </Button>
+                                    </label>
                                     <Button onClick={() => handleAudioChange(audio1)} style={{ backgroundColor: 'rgb(0, 200, 255)', border: 'none', margin: '10px' }} variant="primary">File mẫu 1</Button>
                                     <Button onClick={() => handleAudioChange(audio2)} style={{ backgroundColor: 'rgb(0, 200, 255)', border: 'none', margin: '10px' }} variant="primary">File mẫu 2</Button>
                                 </div>
@@ -43,7 +103,7 @@ function SuDungDichVu() {
 
                             <Col md={6}>
                                 <h6>Âm thanh</h6>
-                                <audio controls style={{ width: '100%' }} src={audioSrc}></audio>
+                                <audio controls style={{ width: '100%' }} src={audioSrc} autoPlay={true}></audio>
                             </Col>
                         </Row>
 
