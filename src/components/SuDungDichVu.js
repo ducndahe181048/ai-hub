60de7ffd4c453f5ff6521adb006ce498c48dd0f3
ducndahe181048 <144119087+ducndahe181048@users.noreply.github.com>
2025-01-09 09:43:42 +0700

    feat: import audio sample file and audio player

--- src/components/SuDungDichVu.js
+++ src/components/SuDungDichVu.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Col, Container, Row } from 'react-bootstrap'
+import audio1 from '../assets/audio.wav'
+import audio2 from '../assets/FPTOpenSpeechData_Set001_V0.1_000001.wav'
 
 function SuDungDichVu() {
+    const [audioSrc, setAudioSrc] = useState('');
+
+    const handleAudioChange = (src) => {
+        setAudioSrc(src);
+    }
+    
     return (
         <div style={{ padding: '20px', textAlign: 'left', margin: '20px' }}>
             <Container>
@@ -24,23 +32,18 @@ function SuDungDichVu() {
                             </Col>
 
                             <Col md={5}>
-                                <h6>Chọn nguồn âm thanh - File (.wav, .mp3) upload dung lượng không quá 20MB</h6>
+                                <h6>Chọn nguồn âm thanh - File (.wav) upload dung lượng không quá 20MB</h6>
                                 <div style={{ padding: '10px' }}>
                                     <Button style={{ backgroundColor: 'rgb(0, 200, 255)', border: 'none', margin: '10px' }} variant="primary">Ghi âm giọng nói</Button>
                                     <Button style={{ backgroundColor: 'rgb(0, 200, 255)', border: 'none', margin: '10px' }} variant="primary">Tải file âm thanh</Button>
-                                    <Button style={{ backgroundColor: 'rgb(0, 200, 255)', border: 'none', margin: '10px' }} variant="primary">File mẫu 1</Button>
-                                    <Button style={{ backgroundColor: 'rgb(0, 200, 255)', border: 'none', margin: '10px' }} variant="primary">File mẫu 2</Button>
+                                    <Button onClick={() => handleAudioChange(audio1)} style={{ backgroundColor: 'rgb(0, 200, 255)', border: 'none', margin: '10px' }} variant="primary">File mẫu 1</Button>
+                                    <Button onClick={() => handleAudioChange(audio2)} style={{ backgroundColor: 'rgb(0, 200, 255)', border: 'none', margin: '10px' }} variant="primary">File mẫu 2</Button>
                                 </div>
                             </Col>
 
                             <Col md={6}>
                                 <h6>Âm thanh</h6>
-                                <audio controls style={{ width: '100%' }}>
-                                    <source src="horse.ogg" type="audio/ogg" />
-                                    <source src="horse.mp3" type="audio/mpeg" />
-                                    <source src="horse.wav" type="audio/wav" />
-                                    Your browser does not support the audio element.
-                                </audio>
+                                <audio controls style={{ width: '100%' }} src={audioSrc}></audio>
                             </Col>
                         </Row>
 
@@ -51,7 +54,7 @@ function SuDungDichVu() {
 
                             <Col md={11}>
                                 <h6>Nội dung văn bản được nhận dạng</h6>
-                                <textarea rows={10} cols={50} style={{ width: '100%', padding: '10px' }} />
+                                <textarea disabled={true} rows={10} cols={50} style={{ width: '100%', padding: '10px' }} />
                             </Col>
                         </Row>
                     </Col>
